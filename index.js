2a1872e4efee3662b7aa6ce51de27b71f08104d2
Rasmus Nørskov <rasmus@norskov.com>
2016-05-17 15:15:49 +0200

    asd

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
-client.takeoff();
-
-client.on('navdata', console.log);
+client.on('navdata', function (e) {
+	if(e.demo) {
+		console.log(e.demo.batteryPercentage)
+		console.log(e.demo.altitude)
+	}
+});
--- systemFuntions.js
+++ systemFuntions.js
@@ -1,61 +1,21 @@
-function drone_land(){
-  client.after(2000, function(){
-    this.stop();
-    this.land();
-  });
+var drone = require('ar-drone');
+var client = drone.createClient();
+
+function drone_fly(params) {
+    
 }
 
-function drone_takeoff(){
-  client.takeoff();
-}
-function drone_fly(var param){
-  switch (param.speed) {
-    case 1: client.after(1000, function{
-      this.front(0,1);
-    })
-    .after(distanceToTime(1,param.distance),function(){
-      this.stop();
-    });
-    break;
 
-    case 2: client.after(1000, function(){
-      this.front(0,2);
-    })
-    .after(distancetoTime(2,param.distance),function(){
-      this.stop();
-    });
-    break;
-
-    case 3: client.after(1000, function(){
-      this.front(0,3);
-    })
-    .after(distancetoTime(3,param.distance),function(){
-      this.stop();
-    });
-    break;
-
-    case 4: client.after(1000, function(){
-      this.front(0,2);
-    })
-    .after(distancetoTime(2,param.distance),function(){
-      this.stop();
-    });
-    break;
-
-    default:
-    throw "Speed for drone.front should be 1-4";
-  }
-}
-function distanceToTime(var speed, var distance){
-  switch (speed) {
-    case 1: return 890.89735313 * distance + 2362.49193028;
-    break;
-    case 2: return 498.82927822 * distance + 1532.83111066;
-    break;
-    case 3:
-    break;
-    case 4:
-    break;
-    default:
-  }
-}
+// function distanceToTime(var speed, var distance){
+//   switch (speed) {
+//     case 1: return 890.89735313 * distance + 2362.49193028;
+//     break;
+//     case 2: return 498.82927822 * distance + 1532.83111066;
+//     break;
+//     case 3:
+//     break;
+//     case 4:
+//     break;
+//     default:
+//   }
+// }
